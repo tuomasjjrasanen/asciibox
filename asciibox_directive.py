e512e3c8431af689828d033995f6f9d110fbdcd0
Tuomas Jorma Juhani Räsänen <tuomasjjrasanen@tjjr.fi>
2012-05-03 21:56:11 +0300

    asciibox directive option: scale

--- asciibox_directive.py
+++ asciibox_directive.py
@@ -9,11 +9,16 @@ class ASCIIBoxDirective(rst.Directive):
 
     required_arguments = 1
     has_content = True
+    option_spec = {
+        'scale': rst.directives.nonnegative_int,
+        }
 
     def run(self):
         self.assert_has_content()
         filename = self.arguments[0] + os.path.extsep + "png"
-        asciibox.render_to_filename("\n".join(self.content), filename)
+        scale = self.options.get("scale", 30)
+        asciibox.render_to_filename("\n".join(self.content), filename,
+                                    scale=scale)
         uri = rst.directives.uri(filename)
         return [docutils.nodes.image(uri=uri)]
 
