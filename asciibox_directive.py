8996a9d34401449e63bd9602cb5005c6dcb17585
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2014-08-22 22:31:38 +0300

    Fix reStructuredText directive to use the updated asciibox module

--- asciibox_directive.py
+++ asciibox_directive.py
@@ -5,20 +5,31 @@ from docutils.parsers import rst
 
 import asciibox
 
+def output_format(argument):
+    return rst.directives.choice(argument, asciibox.OUTPUT_FORMATS)
+
 class ASCIIBoxDirective(rst.Directive):
 
     required_arguments = 1
+    optional_arguments = 2
     has_content = True
     option_spec = {
         'scale': rst.directives.nonnegative_int,
+        'output_format': output_format,
         }
 
     def run(self):
         self.assert_has_content()
-        filename = self.arguments[0] + os.path.extsep + "png"
-        scale = self.options.get("scale", 30)
-        asciibox.render_to_filename("\n".join(self.content), filename,
-                                    scale=scale)
+
+        render_options = {}
+        for key in ASCIIBoxDirective.option_spec:
+            try:
+                render_options[key] = self.options[key]
+            except KeyError:
+                continue
+
+        filename = self.arguments[0]
+        asciibox.render("\n".join(self.content), filename, **render_options)
         uri = rst.directives.uri(filename)
         return [docutils.nodes.image(uri=uri)]
 
